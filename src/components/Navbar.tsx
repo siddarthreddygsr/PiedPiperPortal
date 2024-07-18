0c1f130197a67e19e882b38762067d4c0b025ded
Siddarthreddygsr <siddarthreddygsr@gmail.com>
2024-07-18 19:02:04 +0530

    404 page and gradient changes
    Signed-off-by: Siddarthreddygsr <siddarthreddygsr@gmail.com>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -72,7 +72,7 @@ export default function Header() {
           </Link>
         </nav>
         <div className="flex items-center gap-4">
-          <Button variant="ghost" onClick={toggleTheme}>
+          <Button size="icon" variant="ghost" onClick={toggleTheme}>
             {isDarkMode ? <SunIcon className="h-5 w-5" /> : <MoonIcon className="h-5 w-5" />}
           </Button>
           <Sheet>
@@ -252,23 +252,3 @@ function SunIcon(props: React.SVGProps<SVGSVGElement>) {
     </svg>
   )
 }
-
-// function XIcon(props: React.SVGProps<SVGSVGElement>) {
-//   return (
-//     <svg
-//       {...props}
-//       xmlns="http://www.w3.org/2000/svg"
-//       width="24"
-//       height="24"
-//       viewBox="0 0 24 24"
-//       fill="none"
-//       stroke="currentColor"
-//       strokeWidth="2"
-//       strokeLinecap="round"
-//       strokeLinejoin="round"
-//     >
-//       <path d="M18 6 6 18" />
-//       <path d="m6 6 12 12" />
-//     </svg>
-//   );
-// }
